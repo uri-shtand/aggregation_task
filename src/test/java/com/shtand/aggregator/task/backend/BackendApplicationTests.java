a0fb63cdfe853be870234ec826e698ca75adf283
Uri Shtand <uri.shtand@gmail.com>
2022-08-21 20:29:53 +0300

    get with filter

--- src/test/java/com/shtand/aggregator/task/backend/BackendApplicationTests.java
+++ src/test/java/com/shtand/aggregator/task/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
 package com.shtand.aggregator.task.backend;
 
 import com.google.common.collect.ImmutableList;
-import com.shtand.aggregator.task.backend.model.BaseCase;
-import com.shtand.aggregator.task.backend.model.GetAggregationsResponse;
-import com.shtand.aggregator.task.backend.model.RefreshResponse;
+import com.google.common.collect.ImmutableSet;
+import com.shtand.aggregator.task.backend.model.*;
 import com.shtand.aggregator.task.backend.reader.CrmCaseFetchService;
+import com.shtand.aggregator.task.backend.repo.AggregatedCaseRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +27,9 @@ class BackendApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private AggregatedCaseRepository aggregatedCaseRepository;
+
     @MockBean
     private CrmCaseFetchService crmCaseFetchService;
     private BaseCase baseCase1 = BaseCase.builder()
@@ -39,6 +42,16 @@ class BackendApplicationTests {
 			.providerId(2L)
 			.createdErrorCode(200L)
 			.build();
+
+    AggregatedCase aggregatedCase = AggregatedCase.builder()
+            .caseStatus("test")
+            .id("123")
+            .numberOfSupportCases(1)
+            .caseIds(ImmutableList.of(1234L))
+            .errorCode("555")
+            .providerName("testProvider")
+            .productsAffected(ImmutableSet.of("testProduct"))
+            .build();
 
     @Test
     void contextLoads() {
@@ -56,11 +69,40 @@ class BackendApplicationTests {
 
     @Test
     public void getAggregations() throws Exception {
+        aggregatedCaseRepository.deleteAll();
+        aggregatedCaseRepository.saveAll(ImmutableList.of(aggregatedCase));
         String baseUrl = "http://localhost:" + port;
         ResponseEntity<GetAggregationsResponse> aggregationsResultResponseEntity = restTemplate.getForEntity(baseUrl + CASEAGG_AGGREGATIONS_ALL, GetAggregationsResponse.class);
         assertThat(aggregationsResultResponseEntity.getStatusCodeValue()).isEqualTo(200);
         GetAggregationsResponse aggregationsResultResponseEntityBody = aggregationsResultResponseEntity.getBody();
         assertThat(aggregationsResultResponseEntityBody).isNotNull();
+        AggregatedCaseDto aggregatedCaseDto = aggregationsResultResponseEntityBody.getAggregatedCaseDtos().get(0);
+        assertThat(aggregatedCaseDto.getId()).isEqualTo("123");
+    }
+
+    @Test
+    public void getAggregationsWithFilter_hasResult() throws Exception {
+        aggregatedCaseRepository.deleteAll();
+        aggregatedCaseRepository.saveAll(ImmutableList.of(aggregatedCase));
+        String baseUrl = "http://localhost:" + port;
+        ResponseEntity<GetAggregationsResponse> aggregationsResultResponseEntity = restTemplate.getForEntity(baseUrl + CASEAGG_AGGREGATIONS_ALL+"?providerName=testProvider", GetAggregationsResponse.class);
+        assertThat(aggregationsResultResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        GetAggregationsResponse aggregationsResultResponseEntityBody = aggregationsResultResponseEntity.getBody();
+        assertThat(aggregationsResultResponseEntityBody).isNotNull();
+        AggregatedCaseDto aggregatedCaseDto = aggregationsResultResponseEntityBody.getAggregatedCaseDtos().get(0);
+        assertThat(aggregatedCaseDto.getId()).isEqualTo("123");
+    }
+
+    @Test
+    public void getAggregationsWithFilter_noResult() throws Exception {
+        aggregatedCaseRepository.deleteAll();
+        aggregatedCaseRepository.saveAll(ImmutableList.of(aggregatedCase));
+        String baseUrl = "http://localhost:" + port;
+        ResponseEntity<GetAggregationsResponse> aggregationsResultResponseEntity = restTemplate.getForEntity(baseUrl + CASEAGG_AGGREGATIONS_ALL+"?providerName=dummy", GetAggregationsResponse.class);
+        assertThat(aggregationsResultResponseEntity.getStatusCodeValue()).isEqualTo(200);
+        GetAggregationsResponse aggregationsResultResponseEntityBody = aggregationsResultResponseEntity.getBody();
+        assertThat(aggregationsResultResponseEntityBody).isNotNull();
+        assertThat(aggregationsResultResponseEntityBody.getAggregatedCaseDtos()).isEmpty();
     }
 
 }
